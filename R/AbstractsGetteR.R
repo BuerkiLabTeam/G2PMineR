f015d212459fa7dad20c3251ea8fb81303cfaddf
John M. A. Wojahn <35826002+wojahn@users.noreply.github.com>
2026-06-19 20:55:43 -0600

    Update AbstractsGetteR.R
    Removing broken dependancy

--- R/AbstractsGetteR.R
+++ R/AbstractsGetteR.R
@@ -1,7 +1,7 @@
 #' Gets Abstracts Frm PubMed IDs
 #' @param IDs character or numeric vector: unique abstract IDs
 #' @export
-# Written by John M. A. Wojahn June 2020
+# Written by John M. A. Wojahn June 2020, Updated June 2026 (Awesome AWD)
 # This is Free and Open-Source Software (F.O.S.S.)
 # © J.M.A. Wojahn, S.J. Galla, A.E. Melton, S. Buerki
 # Provided under the GNU AGPLv3 License
@@ -9,55 +9,44 @@
 
 AbstractsGetteR <- function(IDs)
 {
-  if(class(IDs) != "character" && class(IDs) != "numeric")
-  {
+  if (!is.character(IDs) && !is.numeric(IDs)) {
     stop("ERROR: IDs must be a character or numeric vector!")
   }
-  IDs <- as.numeric(as.vector(IDs))
-  # Query pubmed and fetch many results
-  outvec <- c(1:length(IDs))
-  for(l in 1:length(IDs))
-  {
-    Sys.sleep(1)
-    print(sprintf("Processing PubMedID No. %s",l))
-    print(IDs[l])
-    PubMed_Query <- easyPubMed::get_pubmed_ids(IDs[l])
-    if(length(PubMed_Query[["IdList"]][["Id"]]) >= 1)
-    {
-      # Fetch data
-      print("Mining PubMed Data...")
-      my_abstracts_xml <- easyPubMed::fetch_pubmed_data(PubMed_Query,
-                                                        retstart = 0,
-                                                        retmax = 5000)
-      # store Pubmed Records as elements of a list
-      print("Coercing Articles to list...")
-      all_xml <- easyPubMed::articles_to_list(my_abstracts_xml)
-      # Perform operation (use lapply here, no further parameters)
-      print("Converting...")
-      final_df <- do.call(rbind, lapply(all_xml, easyPubMed::article_to_df,
-                                  max_chars = -1, getAuthors = FALSE))
-      print("Converting...")
-      # Perform operation (use lapply here, no further parameters)
-      final_authors_df <- do.call(rbind, lapply(all_xml, easyPubMed::article_to_df,
-                                  max_chars = -1, getAuthors = TRUE))
-      print("Formatting...")
-      if(length(final_authors_df) != 0)
-      {
-        id_abst_journal_year_names_df <- final_authors_df[,c("pmid",
-                                                             "abstract",
-                                                             "year", "journal",
-                                                             "lastname",
-                                                             "firstname")]
-        print("Isolating Abstract...")
-        outvec[l] <- as.character(id_abst_journal_year_names_df[1,2])
-        print("IN PUBMED")
-      }else{
-        print("NOT IN PUBMED VERIFIED")
+  IDs <- as.character(as.vector(IDs))
+  outvec <- character(length(IDs))
+  
+  for (l in 1:length(IDs)) {
+    # 1. Take a full 1.5-second breath to fly completely under NCBI's firewall radar
+    Sys.sleep(1.5) 
+    print(sprintf("Processing PubMedID No. %s: %s", l, IDs[l]))
+    
+    # 2. Build the exact raw E-utilities URL for fetching an abstract in plain text
+    api_url <- paste0("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?",
+                      "db=pubmed&id=", IDs[l], "&retmode=text&rettype=abstract")
+    
+    # 3. Read the web text directly bypassing heavy R package wrappers
+    raw_text <- tryCatch({
+      suppressWarnings(readLines(api_url, warn = FALSE))
+    }, error = function(e) { NULL })
+    
+    # 4. Process the raw text dump
+    if (!is.null(raw_text) && length(raw_text) > 0) {
+      # Collapse the lines into a single block of text
+      full_abstract <- paste(raw_text, collapse = " ")
+      
+      # Clean up any trailing/leading white space
+      full_abstract <- trimws(full_abstract)
+      
+      if (nchar(full_abstract) > 50) {
+        print("Isolating Abstract... IN PUBMED")
+        outvec[l] <- full_abstract
+      } else {
+        print("NOT IN PUBMED VERIFIED (Empty Response)")
         outvec[l] <- "NOT_IN_PUBMED"
       }
-    }else{
-        print("NOT IN PUBMED VERIFIED")
-        outvec[l] <- "NOT_IN_PUBMED"
+    } else {
+      print("NOT IN PUBMED VERIFIED (NCBI Connection Dropped)")
+      outvec[l] <- "NOT_IN_PUBMED"
     }
   }
   return(outvec)
